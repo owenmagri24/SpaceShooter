9c62e40c8f3afe387d62350a8e0961b58d6d7438
owenmagri24 <74650731+owenmagri24@users.noreply.github.com>
2020-11-26 16:01:32 +0100

    Finalized Player
    -Created movement boundaries
    -Created laser object and script

--- Space Shooter/Assets/Scripts/Player.cs
+++ Space Shooter/Assets/Scripts/Player.cs
@@ -16,6 +16,12 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {   
+        CalculateMovement();
+
+    }
+
+    void CalculateMovement()
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
         //Vector3.right == Vector3(1,0,0)
@@ -30,5 +36,30 @@ public class Player : MonoBehaviour
         //option 3
         Vector3 direction = new Vector3(horizontalInput,verticalInput,0);
         transform.Translate(direction * _speed * Time.deltaTime);
+
+        //if the player position on the y axis > 0
+        // if(transform.position.y >= 0)
+        // {
+        //     transform.position = new Vector3(transform.position.x,0,0);
+        // }
+        //if the player position on y axis <= -3.8f
+        // else if(transform.position.y <= -3.8f)
+        // {
+        //     transform.position = new Vector3(transform.position.x,-3.8f,0);
+        // }
+        
+        //Mathf.clamp(the value we wish to restrict, min value, max value)
+        transform.position  =   new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.8f, 0),0);
+
+
+        //if the player position on the x axis > 11 or < -11, send to other side
+        if(transform.position.x >= 11.3f)
+        {
+            transform.position = new Vector3(-11.3f, transform.position.y,0);
+        }
+        else if(transform.position.x <= -11.3f)
+        {
+            transform.position = new Vector3(11.3f, transform.position.y,0);
+        }
     }
 }
